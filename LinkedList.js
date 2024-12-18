fcb55d8f58e3974bdad040601e9a82c6e4dff2c6
Muhammad Khairul Ikhwan <khairulikhwan@Muhammads-MacBook-Air.local>
2024-12-18 12:00:06 +0800

    Add append, prepend, tail, and at(index) functions

--- LinkedList.js
+++ LinkedList.js
@@ -5,4 +5,64 @@ export default class LinkedList {
         this.head = null;
         this.size = 0;
     }
+
+    append(value){
+        const newNode = new Node(value);
+
+        if (this.head == null){
+            this.head = newNode;
+        } else {
+            let current = this.head;
+            while (current.nextNode !== null) {
+                current = current.nextNode;
+            }
+            current.nextNode = newNode;
+        }
+
+        this.size++;
+    }
+
+    prepend(value){
+        const newNode = new Node(value);
+        newNode.nextNode = this.head;
+        this.head = newNode;
+
+        this.size++;
+    }
+
+    size() {
+        return this.size;
+    }
+
+    head() {
+        return this.head;
+    }
+
+    tail() {
+
+        if (this.head == null){
+            return null;
+        } else {
+            let current = this.head;
+            while (current.nextNode !== null){
+                current = current.nextNode;
+            }
+
+            return current;
+        }
+    }
+
+    at(index) {
+        if (index => this.size || index < 0){
+            return null;
+        } else {
+            let current = this.head;
+            let counter = 0;
+            while (counter !== index){
+                current = current.nextNode;
+                counter++;
+            }
+            return current;
+        }
+    }
 }
---
+++ index.js
@@ -0,0 +1,6 @@
+import LinkedList from './LinkedList.js';
+
+const list = new LinkedList();
+list.append("dog");
+
+console.log(list);nod 
